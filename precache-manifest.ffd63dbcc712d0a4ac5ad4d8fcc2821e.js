616dd15469fe799a80094b03e0195b31b48a6da2
Gianluca Maccioni <lukino999@gmail.com>
2020-03-26 12:31:22 +0000

    Updates

---
+++ precache-manifest.ffd63dbcc712d0a4ac5ad4d8fcc2821e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187c3702bd102e41cbde6d995af80b79",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "294bcb51171ac5ff7804",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1e933889b1a9cb0af47",
+    "url": "/react-test/static/js/2.d3b923cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.d3b923cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294bcb51171ac5ff7804",
+    "url": "/react-test/static/js/main.48910775.chunk.js"
+  },
+  {
+    "revision": "40958326a7a5e8d40d0e",
+    "url": "/react-test/static/js/runtime-main.79a7e98c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
